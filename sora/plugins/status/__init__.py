9a3a2f900f6cd375c1569333163d5771fa45c81f
Komorebi <110453675+KomoriDev@users.noreply.github.com>
2024-02-19 22:07:27 +0800

    ✨ 新增状态查询插件 (#42)
    * :sparkles: 新增 status 插件
    
    * :rotating_light: auto fix by pre-commit hooks
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ sora/plugins/status/__init__.py
@@ -0,0 +1,18 @@
+"""运行状态"""
+
+from nonebot import require
+from nonebot.rule import to_me
+
+require("nonebot_plugin_alconna")
+from nonebot_plugin_alconna import Command
+from nonebot_plugin_alconna.uniseg import UniMessage
+
+from .drawer import draw
+
+status = (
+    Command("status", help_text="查看林汐运行状态")
+    .usage("@bot /status\n@bot /状态")
+    .action(lambda: UniMessage.image(raw=draw()))
+    .build(rule=to_me())
+)
+status.shortcut("状态", {"prefix": True})
---
+++ sora/plugins/status/path.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from sora.config import FONT_DIR, IMAGE_DIR
+
+marker_img_path: Path = IMAGE_DIR / "status" / "marker.png"
+bg_img_path: Path = IMAGE_DIR / "status" / "background.png"
+
+baotu_font_path: Path = FONT_DIR / "baotu.ttf"
+spicy_font_path: Path = FONT_DIR / "SpicyRice-Regular.ttf"
+adlam_font_path: Path = FONT_DIR / "AdlamDisplay-Regular.ttf"
